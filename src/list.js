5d1822206de7db56bb11862f248375dec7a8dd55
nikita3112 <bondarchuknana@gmail.com>
2022-10-19 19:42:56 +0300

    edit table id

---
+++ src/list.js
@@ -0,0 +1,30 @@
+export const ItemsList = [
+  {
+    title: "Softwares",
+    data: [
+      {
+        title: "Wondershare Filmora",
+        downloadCount: "77.2K",
+        src: "/icons/WondershareFilmora.png",
+        downloadLink: "https://vk.com"
+      },
+    ]
+  },
+  {
+    title: "Adobe",
+    data: [
+      {
+        title: "Adobe Photoshop",
+        downloadCount: "87.2K",
+        src: "/icons/AdobePhotoshop.png",
+        downloadLink: "https://youtube.com"
+      },
+      {
+        title: "Adobe Acrobat",
+        downloadCount: "63.6K",
+        src: "/icons/AdobeAcrobat.png",
+        downloadLink: "https://youtube.com"
+      }
+    ]
+  }
+]
