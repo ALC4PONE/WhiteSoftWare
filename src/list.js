630683f843969bfbc20eaaa59850442aa562e148
nikita3112 <bondarchuknana@gmail.com>
2022-10-20 00:42:07 +0300

    add new items and photos

--- src/list.js
+++ src/list.js
@@ -5,6 +5,42 @@ export const ItemsList = [
     title: "Softwares",
     data: [
       {
+        title: "Adobe Photoshop",
+        downloadCount: "87.2K",
+        src: "/icons/adobe-photoshop.png",
+        downloadLink: "https://youtube.com"
+      },
+      {
+        title: "Adobe After Effect",
+        downloadCount: "78.4K",
+        src: "/icons/adobe-after-effects.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Adobe Illustrator",
+        downloadCount: "56.2K",
+        src: "/icons/adobe-illustrator.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Adobe Premiere Pro",
+        downloadCount: "77.4K",
+        src: "/icons/adobe-premiere-pro.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Adobr Lightroom",
+        downloadCount: "36.5K",
+        src: "/icons/adobe-lightroom.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Adobe Acrobat",
+        downloadCount: "63.6K",
+        src: "/icons/adobe-acrobat.png",
+        downloadLink: "https://youtube.com"
+      },
+      {
         title: "Wondershare Filmora",
         downloadCount: "77.2K",
         src: "/icons/WondershareFilmora.png",
@@ -13,30 +49,108 @@ export const ItemsList = [
       {
         title: "3DS MAX",
         downloadCount: "87.2K",
-        src: "/icons/brand_autodesk_icon_158012.png",
+        src: "/icons/autodesk.png",
         downloadLink: "https://youtube.com"
       },
+      {
+        title: "Autodesk Autocad",
+        downloadCount: "23.7K",
+        src: "/icons/autodesk-autocad.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Sony Vegas",
+        downloadCount: "31.4K",
+        src: "/icons/сони-вегас.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Cinema 4D",
+        downloadCount: "15.2K",
+        src: "/icons/cinema_4d.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "FL Studio",
+        downloadCount: "101.2K",
+        src: "/icons/fl-studio.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Spotify",
+        downloadCount: "86.7K",
+        src: "/icons/spotify.png",
+        downloadLink: "https://vk.com"
+      }
     ]
   },
-  
+
   {
-    title: "Adobe",
+    title: "Game",
     data: [
       {
-        title: "Adobe Photoshop",
-        downloadCount: "87.2K",
-        src: "/icons/AdobePhotoshop.png",
-        downloadLink: "https://youtube.com"
+        title: "Call of Duty: Warzone",
+        downloadCount: "34.1K",
+        src: "/icons/call-of-duty-warzone.png",
+        downloadLink: "https://vk.com"
       },
       {
-        title: "Adobe Acrobat",
-        downloadCount: "63.6K",
-        src: "/icons/AdobeAcrobat.png",
-        downloadLink: "https://youtube.com"
+        title: "Fortnite",
+        downloadCount: "42.7K",
+        src: "/icons/fortnite.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Roblox",
+        downloadCount: "95.1K",
+        src: "/icons/roblox.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Apex Legends",
+        downloadCount: "24.7K",
+        src: "/icons/apex.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Genshin",
+        downloadCount: "103.4K",
+        src: "/icons/genshin-impact.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Rainbow Six Siege",
+        downloadCount: "47.9K",
+        src: "/icons/rainbow-six.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "GTA V",
+        downloadCount: "87.3K",
+        src: "/icons/gts-5.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Rust",
+        downloadCount: "16.4K",
+        src: "/icons/rust.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Escape From Tarkov",
+        downloadCount: "18.9K",
+        src: "/icons/escape-from-tarkov.png",
+        downloadLink: "https://vk.com"
+      },
+      {
+        title: "Valorant",
+        downloadCount: "11.7K",
+        src: "/icons/valorant.png",
+        downloadLink: "https://vk.com"
       }
     ]
   }
-  
-  
+
+
   // Конец
 ]
