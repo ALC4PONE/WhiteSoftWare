d73fd3304670222ab3ea9196795a636b5cb257b2
nikita3112 <bondarchuknana@gmail.com>
2022-10-20 18:36:45 +0300

    add links to items

--- src/list.js
+++ src/list.js
@@ -8,79 +8,79 @@ export const ItemsList = [
         title: "Adobe Photoshop",
         downloadCount: "87.2K",
         src: "/icons/adobe-photoshop.png",
-        downloadLink: "https://youtube.com"
+        downloadLink: "https://siasky.net/fARwNtRdzhZM7HcN5f_tMDlIcWSf430XqqxRHbzOBDBnjg"
       },
       {
         title: "Adobe After Effect",
         downloadCount: "78.4K",
         src: "/icons/adobe-after-effects.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAR-PYb9ZjxNXIFrI48TzmFe8QJzfybIr3jpfCGAmv4k7w"
       },
       {
         title: "Adobe Illustrator",
         downloadCount: "56.2K",
         src: "/icons/adobe-illustrator.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAS8CgyS5ciwxTUIAUVKECrOn4tfjsT82M4W33lM-UTk_A"
       },
       {
         title: "Adobe Premiere Pro",
         downloadCount: "77.4K",
         src: "/icons/adobe-premiere-pro.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAS-dqJsZF3jsSSDsel-M9rsBB3fui_d_MT-wlzbnVcfug"
       },
       {
         title: "Adobe Lightroom",
         downloadCount: "36.5K",
         src: "/icons/adobe-lightroom.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fASABWJ9zts14S7N5PTG1jRxakCqT3o-add_5Sn70noIZw"
       },
       {
         title: "Adobe Acrobat",
         downloadCount: "63.6K",
         src: "/icons/adobe-acrobat.png",
-        downloadLink: "https://youtube.com"
+        downloadLink: "https://siasky.net/fATrjAoWRIUgWo_JiCpTkGvwHXGBxJU9zk9yBq6aZHS3Bg"
       },
       {
         title: "Wondershare Filmora",
         downloadCount: "77.2K",
         src: "/icons/WondershareFilmora.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAT6RRMMjVMJXIbM7x7cNSmIlXC6_1aL3mnl2Dx1uV0rcQ"
       },
       {
-        title: "3DS MAX",
+        title: "Autodeck",
         downloadCount: "87.2K",
         src: "/icons/autodesk.png",
-        downloadLink: "https://youtube.com"
+        downloadLink: "https://siasky.net/fAQYE1P4r8rhLctqeTek8h_psX0uj8eLJblvIDnYdZ1iSg"
       },
       {
         title: "Autodesk Autocad",
         downloadCount: "23.7K",
         src: "/icons/autodesk-autocad.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAQSeQ6V3CwLRMEAz3Q8hkW-VnBPVMqgG995F4ERzp6acQ"
       },
       {
         title: "Sony Vegas",
         downloadCount: "31.4K",
         src: "/icons/сони-вегас.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAS5dbWu9N5ZBFKVt8UgdiW_JGBFWBlrCBIRWSrHBxwLtQ"
       },
       {
         title: "Cinema 4D",
         downloadCount: "15.2K",
         src: "/icons/cinema_4d.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAQ9OnaPmacvJi8rzfZPNc712LOangfvhlOv0WUi1Hlr7g"
       },
       {
         title: "FL Studio",
         downloadCount: "101.2K",
         src: "/icons/fl-studio.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fATcmkCXhO0ErC8FkWhfZwS_1kzkouGBnPGT-vTXWCShDg"
       },
       {
         title: "Spotify",
         downloadCount: "86.7K",
         src: "/icons/spotify.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fASjGwHlE55mx2MKGyxRyxfr0VCgNnjzdALWM75kUt_Vvg"
       }
     ]
   },
@@ -92,61 +92,61 @@ export const ItemsList = [
         title: "Call of Duty: Warzone",
         downloadCount: "34.1K",
         src: "/icons/call-of-duty-warzone.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAThWIopE-8nzXnK1mumgK7PfRwpee3EIXjzbFtgnyVhMw"
       },
       {
         title: "Fortnite",
         downloadCount: "42.7K",
         src: "/icons/fortnite.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fASAe_rQTBO-ctryFi5SGaAgsEcPKrWLmh4Fmfi5GoStVQ"
       },
       {
         title: "Roblox",
         downloadCount: "95.1K",
         src: "/icons/roblox.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAQdhKIw9jAOAJyqpct2ab2kLPo5BaAxLEo3yOE9e2zZAg"
       },
       {
         title: "Apex Legends",
         downloadCount: "24.7K",
         src: "/icons/apex.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fATxaBOKVAk3O8KRZYXF6Sf-ARN40pXR0N54l6Rm7Dx3sg"
       },
       {
-        title: "Genshin",
+        title: "Genshin Impact",
         downloadCount: "103.4K",
         src: "/icons/genshin-impact.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fARV57R9QuAJelhB2h-171w5Y1AMCxCQxe3oqf1z6p-tEw"
       },
       {
         title: "Rainbow Six Siege",
         downloadCount: "47.9K",
         src: "/icons/rainbow-six.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fASEBqP7tCpcyEuCasaWmF_B1J4c4Ftjla-iAFy1xEio8Q"
       },
       {
         title: "GTA V",
         downloadCount: "87.3K",
         src: "/icons/gta-5.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fARuOd0YjmcMtyOFAxTxNqZQ11q14DrfTOnjvw_C8yH9-A"
       },
       {
         title: "Rust",
         downloadCount: "16.4K",
         src: "/icons/rust.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fARzVwSMo9eP1pQfKhJCsntqbn68M2f1cxzQ_sz60lrifA"
       },
       {
         title: "Escape From Tarkov",
         downloadCount: "18.9K",
         src: "/icons/escape-from-tarkov.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fARNh42j4gcpdeMsHhoR-ukdWPpT3WpgRJLBSNWFi4q6AQ"
       },
       {
         title: "Valorant",
         downloadCount: "11.7K",
         src: "/icons/valorant.png",
-        downloadLink: "https://vk.com"
+        downloadLink: "https://siasky.net/fAQ_epMQ_mBYIqut69hOVSw9Am8ST_RdelQyO35A7AIrhA"
       }
     ]
   }
