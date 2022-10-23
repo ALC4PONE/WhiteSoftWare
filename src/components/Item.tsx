cc6803986e98210e2db664ce29014dd4a158f454
nikita3112 <bondarchuknana@gmail.com>
2022-10-23 16:10:20 +0300

    feat: add popup

---
+++ src/components/Item.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Popup from './Popup.tsx'
+
+
+interface Item {
+  title: string,
+  src: string,
+  downloadCount: string,
+  downloadLink: string
+}
+
+export default function Item({ title, src, downloadCount, downloadLink, description }: Item) {
+  const [isShow, setIsShow] = useState(false)
+
+  const handleClick = () => setIsShow(true)
+  const closePopup = () => setIsShow(false)
+
+  return (
+    <div className="flex justify-between items-center w-full py-3">
+      <img className="w-12 sm:w-16" src={src} />
+      <div className="w-full text-start pl-2 sm:pl-5">
+        <h3 className="font-bold text-sm sm:text-base">{title}</h3>
+        <p className="font-light text-gray-400 text-xs sm:text-base">{downloadCount} downloads</p>
+      </div>
+      <button
+        className="bg-indigo-100 py-1 sm:py-2 px-2 sm:px-4 rounded-full text-xs sm:text-base text-blue-600 hover:scale-105 transition duration-150 ease-in-out"
+        onClick={handleClick}
+      >
+        Download
+      </button>
+      <Popup
+        title={title}
+        src={src}
+        downloadCount={downloadCount}
+        downloadLink={downloadLink}
+        description={description}
+        isShow={isShow}
+        closePopup={closePopup}
+      />
+    </div>
+  )
+}
---
+++ src/components/Popup.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { XMarkIcon } from '@heroicons/react/20/solid/index.js';
+
+
+interface Info {
+  src: string,
+  title: string,
+  downloadCount: string,
+  description: string,
+  downloadLink: string,
+  isShow: boolean,
+  closePopup: Function
+}
+
+export default function Popup({ src, title, downloadCount, description, downloadLink, isShow, closePopup }: Info) {
+  if (isShow) {
+    return (
+
+      < div
+        className="fixed top-0 left-0 z-20 w-full h-full bg-black/50 flex justify-center items-center"
+        onClick={closePopup}
+      >
+        <div
+          className="flex flex-col bg-white w-2/6 p-10 relative"
+          onClick={e => e.stopPropagation()}
+        >
+          <XMarkIcon
+            className="absolute top-5 right-5 w-8 cursor-pointer"
+            onClick={closePopup}
+          />
+          <div
+            className="flex mb-5"
+          >
+            <img
+              src={src}
+              className="pr-3"
+            />
+            <div>
+              <h2 className="font-mplus font-bold text-xl">
+                {title}
+              </h2>
+              <p className="font-mplus font-light">
+                {downloadCount} downloads
+              </p>
+            </div>
+          </div>
+          <p className="font-mplus font-light text-md mb-5">
+            {description}
+          </p>
+          <div
+            className="bg-indigo-100 px-5 py-3 mb-5 text-blue-500"
+          >
+            Password for the archive: 2022
+          </div>
+          <a
+            className="bg-blue-500 self-end py-2 px-3 text-white rounded-md cursor-pointer transition duration-150 ease-in-out hover:scale-105"
+            href={downloadLink}
+          >
+            Download
+          </a>
+        </div>
+      </div >
+
+    )
+  }
+}
