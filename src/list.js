8e4c1cebfa07b3b12a3ab0bbde31a31db635069a
purgesofts <116493025+purgesofts@users.noreply.github.com>
2022-10-26 16:02:32 +0300

    Update list.js

--- src/list.js
+++ src/list.js
@@ -38,7 +38,7 @@ export const ItemsList = [
         title: "Adobe Acrobat",
         downloadCount: "63.6K",
         src: "/icons/adobe-acrobat.png",
-        downloadLink: "https://filebin.net/7ocnr0nz2racwk4m/Adobe_Acrobat_Crack.zip"
+        downloadLink: "https://filebin.net/xwv27n639grba7a9/Adobe_Acrobat_Crack.zip"
       },
       {
         title: "Wondershare Filmora",
